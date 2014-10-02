85ec56cca05d1c3cf78c3e613a4ec5bcf5e4ba2e
g-rocket <g.rocket1@gmail.com>
2014-10-02 00:48:13 -0400

    Start chunk

--- src/voxels/generate/Chunk.java
+++ src/voxels/generate/Chunk.java
@@ -1,8 +1,15 @@
 package voxels.generate;
 
+import voxels.map.Coord3;
+
 /*
  * 
  */
 public class Chunk {
-
+	public static final int XLENGTH = 16, YLENGTH = 16, ZLENGTH = 16;
+	public final Coord3 position;
+	public final ChunkBrain brain;
+	private final TerrainMap map;
+	public boolean meshBuilding;
+	public boolean meshDirty;
 }
