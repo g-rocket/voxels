b2f02776790d71ccbc946cc709a0a100dd652131
g-rocket <g.rocket1@gmail.com>
2014-10-16 01:00:31 -0400

    start on arithmetic stuff

--- src/voxels/map/Coord3.java
+++ src/voxels/map/Coord3.java
@@ -14,4 +14,20 @@ public class Coord3 {
 	public Vector3f asVector() {
 		return new Vector3f(x,y,z);
 	}
+	
+	public Coord3 plus(Coord3 c) {
+		return new Coord3(x+c.x, y+c.y, z+c.z);
+	}
+	
+	public static Coord3 add(Coord3 c1, Coord3 c2) {
+		return c1.plus(c2);
+	}
+	
+	public Coord3 minus(Coord3 c) {
+		return new Coord3(x-c.x, y-c.y, z-c.z);
+	}
+	
+	public static Coord3 subtract(Coord3 c1, Coord3 c2) {
+		return c1.minus(c2);
+	}
 }
