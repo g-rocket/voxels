4647f8f50fbd0648a34e1705fd9230f5f160107b
g-rocket <g.rocket1@gmail.com>
2014-10-16 00:59:56 -0400

    Rename TerrainMap to WorldMap

--- src/voxels/map/WorldMap.java
+++ src/voxels/map/WorldMap.java
@@ -2,8 +2,11 @@ package voxels.map;
 
 import static voxels.map.BlockType.*;
 
-public class TerrainMap {
-	
+
+/*
+ * Deals with the overarching structure (knows about ALL the chunks)
+ */
+public class WorldMap {
 	public byte getBlock(Coord3 woco) {
 		return (byte) DIRT.ordinal();
 	}
