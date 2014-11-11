e9f7e1cfa7f5dc0fb859286a6188a3f010a9044c
g-rocket <g.rocket1@gmail.com>
2014-11-11 01:51:18 -0500

    some stuff about geometry bounds... didn't change anything

--- src/voxels/generate/Chunk.java
+++ src/voxels/generate/Chunk.java
@@ -87,7 +87,13 @@ public class Chunk extends AbstractControl {
         }
         
         MeshBuilder.applyMeshSet(mset, getGeometry().getMesh());
-
+        /* GAVIN:
+         * THE FOLLOWING COMMENTED OUT CODE IS A 'COUSIN' OF mesh.updateBound().
+         * THAT'S THE GOOD NEWS. BAD NEWS IS IT DOESN'T SOLVE THE DISAPPEARING MESH PROBLEM!
+         * HENCE, JUST LEAVING IT HERE FOR YOUR REFERENCE (IT MIGHT BE GOOD TO KNOW ABOUT NONETHELESS). 
+         * PLEASE DELETE THIS NOTE AND THE LINE AS NEEDED. --MEDDLER
+         */
+        // getGeometry().updateModelBound();
 	}
 
 	@Override
