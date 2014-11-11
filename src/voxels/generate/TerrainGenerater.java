0acc0f5bb48ddad2bfbb3cc4c7696da6441c703f
g-rocket <g.rocket1@gmail.com>
2014-11-11 01:50:25 -0500

    better terrain generation

--- src/voxels/generate/TerrainGenerater.java
+++ src/voxels/generate/TerrainGenerater.java
@@ -7,9 +7,13 @@ import voxels.map.*;
 public class TerrainGenerater {
 	Random r = new Random();
 	public BlockType getBlockAtPosistion(Coord3 pos) {
-		if(r.nextInt(4) < 1) {
-			return BlockType.values()[r.nextInt(BlockType.values().length)];
-		}
+		r.setSeed((long)pos.x | ((long)pos.y << 32));
+		r.nextLong(); // makes it look more random
+		if(pos.z < 1 + r.nextInt(4)) return BlockType.UNKNOWN;
+		if(pos.z < 4 + r.nextInt(8)) return BlockType.ROCK;
+		int dtop = 8 + r.nextInt(8);
+		if(pos.z < dtop) return BlockType.DIRT;
+		if(pos.z == dtop) return BlockType.GRASS;
 		return BlockType.AIR;
 	}
 }
