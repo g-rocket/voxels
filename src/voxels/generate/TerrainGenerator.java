a0f08ccfe16870aac147ad694b90bc7d42e1e74d
g_rocket <g.rocket1@gmail.com>
2014-11-20 00:39:50 -0500

    delete commented-out code

--- src/voxels/generate/TerrainGenerator.java
+++ src/voxels/generate/TerrainGenerator.java
@@ -6,7 +6,6 @@ import java.util.*;
 import voxels.block.*;
 import voxels.map.*;
 
-import com.sudoplay.joise.*;
 import com.sudoplay.joise.module.*;
 
 public class TerrainGenerator {
@@ -32,13 +31,5 @@ public class TerrainGenerator {
 
 	public BlockType getBlockAtPosistion(Coord3 pos) {
 		return BlockType.getBlock((int)terrain.get(pos.x, pos.y, pos.z));
-		/*r.setSeed((long)pos.x | ((long)pos.y << 32));
-		r.nextLong(); // makes it look more random
-		if(pos.z < 1 + r.nextInt(4)) return BlockType.UNKNOWN;
-		if(pos.z < 4 + r.nextInt(8)) return BlockType.ROCK;
-		int dtop = 8 + r.nextInt(8);
-		if(pos.z < dtop) return BlockType.DIRT;
-		if(pos.z == dtop) return BlockType.GRASS;
-		return BlockType.AIR;*/
 	}
 }
