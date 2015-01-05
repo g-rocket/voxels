3bcf947af0306fee5e806f577da5d1d8c488820d
g-rocket <g.rocket1@gmail.com>
2015-01-05 13:52:53 -0500

    not really sure what (more work on MainPlayer)

--- src/voxels/entity/MainPlayer.java
+++ src/voxels/entity/MainPlayer.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 import voxels.map.*;
 
+import com.jme3.bullet.*;
 import com.jme3.bullet.control.*;
 import com.jme3.export.*;
 import com.jme3.input.controls.*;
@@ -14,15 +15,15 @@ import com.jme3.scene.control.*;
 
 public class MainPlayer extends BetterCharacterControl implements Player, ActionListener {
 	private final Camera camera;
-	
+
 	private float sideSpeed = 1;
 	private float frontSpeed = 2;
 	private float backSpeed = .5f;
-	
+
 	public MainPlayer(Camera camera) {
 		super(.5f, 1.8f, 1f);
 		setGravity(new Vector3f(0,1,0));
-	    setPhysicsLocation(new Vector3f(0, 20, 0));
+		setPhysicsLocation(new Vector3f(0, 20, 0));
 		this.camera = camera;
 	}
 
@@ -31,6 +32,13 @@ public class MainPlayer extends BetterCharacterControl implements Player, Action
 		return super.getSpatialTranslation();
 	}
 
+	@Override
+	public void physicsTick(PhysicsSpace space, float tpf) {
+		super.physicsTick(space, tpf);
+		camera.setLocation(super.getSpatialTranslation());
+		camera.setRotation(super.getSpatialRotation());
+	};
+	
 	@Override
 	public void onAction(String name, boolean isPressed, float tpf) {
 		Vector3f walkDirection = new Vector3f(0,0,0);
